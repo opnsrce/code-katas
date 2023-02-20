b9e12bfb7e3b1e006394e0dc39e71223b24d9f1d
Levi Hackwith <levi.hackwith@gmail.com>
2023-02-19 16:42:21 -0800

    Add file description to Palindrome kata

--- src/palindrome/index.ts
+++ src/palindrome/index.ts
@@ -1,3 +1,8 @@
+/**
+ * @description: Write a function that takes in a string and returns whether or
+ * not it's a palindrome
+ */
+
 const main = (string: string): boolean => {
     const regExp = /[^a-z0-9]/g;
 
