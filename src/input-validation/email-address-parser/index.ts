0e5da8f8f095ea0d697a5706b116156c44148706
Levi Hackwith <levi.hackwith@gmail.com>
2023-02-25 13:27:01 -0800

    Add missing file description to emailAddressParser

--- src/input-validation/email-address-parser/index.ts
+++ src/input-validation/email-address-parser/index.ts
@@ -1,3 +1,18 @@
+/**
+ * @description Write a function that takes an array of email addresses and
+ * returns an object with two arrays: one containing valid email addresses, and
+ * another containing invalid ones.
+ *
+ * An email address is considered valid if it meets the following criteria:
+ * - Contains a username, followed by the "@" symbol, followed by a domain name,
+ *   followed by a top-level domain (e.g. ".com").
+ * - The username and domain name can contain lowercase letters, numbers,
+ *   underscores, and periods.
+ * - The top-level domain must be at least two characters long.
+ *
+ * The function should throw a TypeError if the input is not an array of
+ * strings.
+ */
 const emailAddressParser = (emails: string[]): object => {
     const valid: string[] = [];
     const invalid: string[] = [];
